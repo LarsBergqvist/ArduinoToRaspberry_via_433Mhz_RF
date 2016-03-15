182a8348fe0b2481d7204aeb1cfbc56c418b04c1
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2016-03-15 22:04:58 +0000

    Added multiple measurement types (light, temp, humidity)

--- Raspberry/receiver.py
+++ Raspberry/receiver.py
@@ -2,11 +2,12 @@
 # See pi_switch readme for details on setup
 
 from pi_switch import RCSwitchReceiver
+import time
 
 receiver = RCSwitchReceiver()
 receiver.enableReceive(2)
 
-acceptedIds = [3,4]
+acceptedTypes = { 1 : "Light", 2 : "Temp [C]", 3: "Humidity [%]" }
 prev_value = 0L
 while True:
     if receiver.available():
@@ -18,7 +19,7 @@ while True:
         
         # decode byte3
         byte3 = (0xFF000000 & value) >> 24
-        arduinoID = int((0xF0 & byte3) >> 4)
+        typeID = int((0xF0 & byte3) >> 4)
         seqNum = int((0x0F & byte3))
 
         # decode byte2 and byte1
@@ -27,19 +28,25 @@ while True:
         # decode byte0
         checkSum = int((0x000000FF & value))
 
-        calculatedCheckSum = 0xFF & (arduinoID + seqNum + data)
+        calculatedCheckSum = 0xFF & (typeID + seqNum + data)
 
         # Sanity checks on received data
+        correctData = True
         if calculatedCheckSum != checkSum:
-            print("Incorrect checksum!")
+#            print("Incorrect checksum!")
+            correctData = False
         elif data > 1023:
-            print("Incorrect data!")
-        elif arduinoID not in acceptedIds:
-            print("Incorrect ID!")
+#            print("Incorrect data!")
+            correctData = False
+        elif typeID not in acceptedTypes:
+#            print("Incorrect ID!")
+            correctData = False
         elif seqNum > 15:
-            print("Incorrect SeqNum")
-        else:
-            print("OK data!", "ArduinoID:",arduinoID,"SeqNum:",seqNum,"Measurement:",data)
+#            print("Incorrect SeqNum")
+            correctData = False
+
+        if correctData:
+            print(str.format("{0}: {1}={2} SeqNum={3}",time.ctime(),acceptedTypes[typeID],data,seqNum))
             prev_value = value
 
         receiver.resetAvailable()
