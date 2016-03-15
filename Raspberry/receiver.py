49f089f31b1e2b179b24d7bb5e36fa48270b692c
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2016-03-15 21:23:15 +0000

    Added sanity checks on received data

--- Raspberry/receiver.py
+++ Raspberry/receiver.py
@@ -6,10 +6,16 @@ from pi_switch import RCSwitchReceiver
 receiver = RCSwitchReceiver()
 receiver.enableReceive(2)
 
+acceptedIds = [3,4]
+prev_value = 0L
 while True:
     if receiver.available():
         value = receiver.getReceivedValue()
 
+        if value == prev_value:
+            # we have already seen this measurement, so ignore it
+            continue
+        
         # decode byte3
         byte3 = (0xFF000000 & value) >> 24
         arduinoID = int((0xF0 & byte3) >> 4)
@@ -23,23 +29,18 @@ while True:
 
         calculatedCheckSum = 0xFF & (arduinoID + seqNum + data)
 
+        # Sanity checks on received data
         if calculatedCheckSum != checkSum:
             print("Incorrect checksum!")
-            print(calculatedCheckSum,checkSum)
         elif data > 1023:
             print("Incorrect data!")
-        elif arduinoID > 15:
+        elif arduinoID not in acceptedIds:
             print("Incorrect ID!")
         elif seqNum > 15:
             print("Incorrect SeqNum")
-
-        print(arduinoID,seqNum,data)
-
-#        print("ArduinoID: ", arduinoID)
-#        print("SeqNum: ", seqNum)
-#        print("Data: ", data)
-#        print("CheckSum: ", checkSum)
-
+        else:
+            print("OK data!", "ArduinoID:",arduinoID,"SeqNum:",seqNum,"Measurement:",data)
+            prev_value = value
 
         receiver.resetAvailable()
         
