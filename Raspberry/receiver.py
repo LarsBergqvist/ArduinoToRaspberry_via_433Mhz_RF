25fa9e42eee6651ae690c38baedc41a4343d78ee
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2016-03-15 20:29:24 +0000

    Added first version of receiver on Raspberry side

---
+++ Raspberry/receiver.py
@@ -0,0 +1,45 @@
+from pi_switch import RCSwitchReceiver
+
+receiver = RCSwitchReceiver()
+receiver.enableReceive(2)
+
+num = 0
+prev_value = 0
+collectedValues = {}
+while True:
+    if receiver.available():
+        value = receiver.getReceivedValue()
+
+        # decode byte3
+        byte3 = (0xFF000000 & value) >> 24
+        arduinoID = int((0xF0 & byte3) >> 4)
+        seqNum = int((0x0F & byte3))
+
+        # decode byte2 and byte1
+        data = int((0x00FFFF00 & value) >> 8)
+
+        # decode byte0
+        checkSum = int((0x000000FF & value))
+
+        calculatedCheckSum = 0xFF & (arduinoID + seqNum + data)
+
+        if calculatedCheckSum != checkSum:
+            print("Incorrect checksum!")
+            print(calculatedCheckSum,checkSum)
+        elif data > 1023:
+            print("Incorrect data!")
+        elif arduinoID > 15:
+            print("Incorrect ID!")
+        elif seqNum > 15:
+            print("Incorrect SeqNum")
+
+        print(arduinoID,seqNum,data)
+
+#        print("ArduinoID: ", arduinoID)
+#        print("SeqNum: ", seqNum)
+#        print("Data: ", data)
+#        print("CheckSum: ", checkSum)
+
+
+        receiver.resetAvailable()
+        
